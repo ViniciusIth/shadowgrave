be81f879178d85d3079cb189b14e0c36a2688d15
ViniciusIth <vini.ithalo@hotmail.com>
2023-12-30 21:12:24 -0300

    first commit

---
+++ src/main/java/cloud/viniciusith/shadowgrave/ShadowGrave.java
@@ -0,0 +1,16 @@
+package cloud.viniciusith.shadowgrave;
+
+import net.fabricmc.api.ModInitializer;
+
+import java.util.logging.Logger;
+
+public class ShadowGrave implements ModInitializer {
+    public static final String MOD_ID = "shadowgrave";
+    public static final Logger LOGGER = Logger.getLogger(MOD_ID);
+
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Hello from " + MOD_ID);
+    }
+}
---
+++ src/main/java/cloud/viniciusith/shadowgrave/client/ShadowGraveClient.java
@@ -0,0 +1,11 @@
+package cloud.viniciusith.shadowgrave.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShadowGraveClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
