2279dbb3f1d8892988e480f5e66bb0428215210f
ViniciusIth <vini.ithalo@hotmail.com>
2024-01-14 18:49:13 -0300

    fix: post death behavior not working

--- src/main/java/cloud/viniciusith/shadowgrave/mixin/LivingEntityMixin.java
+++ src/main/java/cloud/viniciusith/shadowgrave/mixin/LivingEntityMixin.java
@@ -14,11 +14,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class LivingEntityMixin {
     @Inject(method = "drop", at = @At(value = "INVOKE",
             target = "Lnet/minecraft/entity/LivingEntity;dropInventory()V",
-            shift = At.Shift.BEFORE))
+            shift = At.Shift.BEFORE), cancellable = true)
     private void shadowgrave$replaceWithShadow(DamageSource source, CallbackInfo ci) {
         if (((Object) this) instanceof ServerPlayerEntity player) {
             ShadowGraveMod.LOGGER.info("Shadow grave dropped by player " + player.getGameProfile().getName());
             GraveUtils.spawnShadow(player, player.getPos(), source);
+
+            ci.cancel();
         }
     }
 }
